3aa818dc007442100a02a83c98025cb8d8d3e3be
Fernando Moreira <nandomoreira.me@gmail.com>
2020-02-13 13:27:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gatsby WordPress GraphQL
+
+- [Gatsby](https://www.gatsbyjs.org/)
+- [WordPress](https://wordpress.org/)
+- [GraphQL API for WordPress](https://www.wpgraphql.com/)
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { graphql } from 'gatsby'
+
+const IndexPage = ({data}) => (
+  <>
+    <h1>Hello Gatsby</h1>
+  </>
+)
+
+export default IndexPage
+
+export const pageQuery = graphql`
+`
